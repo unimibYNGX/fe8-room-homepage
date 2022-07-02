b60597dc3aa06561912771db2e29e1860a2a15c1
Yang <j.xiang@campus.unimib.it>
2022-07-02 11:09:57 +0200

    maybe add more animations?

---
+++ js/index.js
@@ -0,0 +1,33 @@
+var page = 1;
+
+function move(direction) {
+    if (direction == 'prev') {
+        if (page == 1) 
+            page = 3;
+        else 
+            page--;
+    }
+    if (direction == 'next') {
+        if (page == 3)
+            page = 1;
+        else
+            page++;
+    }
+    showPage(page);
+}
+
+function hide(id) {
+    document.getElementById(id).classList.add('hide');
+}
+function show(id) {
+    document.getElementById(id).classList.remove('hide');
+}
+
+function showPage(n) {
+    for (let index = 1; index <= 3; index++) {
+        hide('img-hero-' + index)
+        hide('block1-' + index);
+    }
+    show('img-hero-' + n)
+    show('block1-' + n)
+}
