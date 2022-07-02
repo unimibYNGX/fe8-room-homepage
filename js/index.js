9dc8cea04efd17cdac422f46c69bdebb400379df
Yang <j.xiang@campus.unimib.it>
2022-07-02 16:47:42 +0200

    format document

--- js/index.js
+++ js/index.js
@@ -2,33 +2,29 @@ var page = 1;
 showPage(page);
 
 function move(direction) {
-    if (direction == 'prev') {
-        if (page == 1) 
-            page = 3;
-        else 
-            page--;
-    }
-    if (direction == 'next') {
-        if (page == 3)
-            page = 1;
-        else
-            page++;
-    }
-    showPage(page);
+  if (direction == "prev") {
+    if (page == 1) page = 3;
+    else page--;
+  }
+  if (direction == "next") {
+    if (page == 3) page = 1;
+    else page++;
+  }
+  showPage(page);
 }
 
 function hide(id) {
-    document.getElementById(id).classList.add('hide');
+  document.getElementById(id).classList.add("hide");
 }
 function show(id) {
-    document.getElementById(id).classList.remove('hide');
+  document.getElementById(id).classList.remove("hide");
 }
 
 function showPage(n) {
-    for (let index = 1; index <= 3; index++) {
-        hide('img-hero-' + index)
-        hide('block1-' + index);
-    }
-    show('img-hero-' + n)
-    show('block1-' + n)
+  for (let index = 1; index <= 3; index++) {
+    hide("img-hero-" + index);
+    hide("block1-" + index);
+  }
+  show("img-hero-" + n);
+  show("block1-" + n);
 }
